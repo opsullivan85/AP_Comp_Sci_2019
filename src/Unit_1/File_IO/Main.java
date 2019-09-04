bc59c10b6b757dead9d75710f336363a944c9104
Owen Sullivan <opsullivan85@gmail.com>
2019-09-04 13:49:58 -0400

    File_IO

---
+++ src/Unit_1/File_IO/Main.java
@@ -0,0 +1,29 @@
+package Unit_1.File_IO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("numbers.txt");
+        Scanner s = new Scanner(f);
+        evenNumbers(s);
+    }
+
+    public static void evenNumbers(Scanner s){
+        int nums = 0 ;
+        int sum = 0;
+        int evens = 0;
+        int val;
+        while(s.hasNextInt()){
+            val = s.nextInt();
+            sum += val;
+            nums++;
+            evens += 1 - (val % 2);
+        }
+        System.out.printf("%d numbers, sum = %d\n", nums, sum);
+        System.out.printf("%d evens (%f%%)\n", evens, evens / (double)nums * 100);
+    }
+}
